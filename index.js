da7620af8066645c493dd7dcab02463f1b25cb31
w1se <w1se@MacBook.local>
2022-07-16 20:15:05 +0300

    Pm-plus modal updated. Added some inputs.

--- index.js
+++ index.js
@@ -15,16 +15,57 @@ window.onresize();
 // View
 function addSession(){
     const modalContainer = document.createElement('div');
-    const modal = document.createElement('div');
-    
+    const modal = document.createElement('form');
+
+    const modalBody = document.createElement('div');
+    const modalButtons = document.createElement('div');
+
     const modalHeader = document.createElement('div');
+    const modalConfigsCont = document.createElement('div');
+    
+    const confPmNumCont = document.createElement('div');
+    const confPmNumContDesc = document.createElement('div');
+    const confPmNumContInput = document.createElement('input');
+
+    const confTimePerPm = document.createElement('div');
+    const confTimePerPmDesc = document.createElement('div');
+    const confTimePerPmInput = document.createElement('input');
+    
     modalHeader.textContent = 'Add session';
+
+    confPmNumContDesc.textContent = 'Num of Pomodoros: ';
+    confPmNumContInput.type = 'number';
+    confPmNumContInput.id = 'input-pm-num';
+    confPmNumContInput.min = 3;
+    confPmNumContInput.max = 45;
+
+    confTimePerPmDesc.textContent = 'Time per Pomodoro';
+    confTimePerPmInput.type = 'range';
+    confTimePerPmInput.min = '10';
+    confTimePerPmInput.max = '60';
+    confTimePerPmInput.value = '25';
     
     modalContainer.classList.add('modal-cont');
     modal.classList.add('modal');
     modalHeader.classList.add('modal-head');
+    modalConfigsCont.classList.add('modal-configs');
+    confPmNumCont.classList.add('conf-pm');
+    confTimePerPm.classList.add('conf-pm');
 
-    modal.appendChild(modalHeader);
+    confTimePerPm.appendChild(confTimePerPmDesc);
+    confTimePerPm.appendChild(confTimePerPmInput);
+
+    confPmNumCont.appendChild(confPmNumContDesc);
+    confPmNumCont.appendChild(confPmNumContInput);
+    
+    modalConfigsCont.appendChild(confPmNumCont);
+    modalConfigsCont.appendChild(confTimePerPm);
+
+    modalBody.appendChild(modalHeader);
+    modalBody.appendChild(modalConfigsCont);
+
+    modal.appendChild(modalBody);
+    modal.appendChild(modalButtons);
     modalContainer.appendChild(modal);
     document.body.appendChild(modalContainer);
 }
