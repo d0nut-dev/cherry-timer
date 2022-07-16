25ad8152fe4f14e85596f2a8c7447d7a35700612
w1se <w1se@MacBook.local>
2022-07-16 18:45:12 +0300

    Added pm-plus button functionality. Modal's CSS provided.

--- index.js
+++ index.js
@@ -3,13 +3,34 @@ window.onresize = function() {
     document.body.height = window.innerHeight;
 }
 window.onresize();
+
+
+
+
+
 // Model
 
 // Controller
 
-
 // View
-function render(){
+function addSession(){
+    const modalContainer = document.createElement('div');
+    const modal = document.createElement('div');
+    
+    const modalHeader = document.createElement('div');
+    modalHeader.textContent = 'Add session';
+    
+    modalContainer.classList.add('modal-cont');
+    modal.classList.add('modal');
+    modalHeader.classList.add('modal-head');
 
+    modal.appendChild(modalHeader);
+    modalContainer.appendChild(modal);
+    document.body.appendChild(modalContainer);
+}
+
+function render(){
+    const plusButton = document.getElementById('pm-plus');
+    plusButton.onclick = addSession;
 }
 render();
